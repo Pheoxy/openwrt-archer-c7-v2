449e244ca1e61aaad9a502fb9b3b162788d74ecf
Daniel Hancock <pheoxy@gmail.com>
2019-05-20 23:19:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,89 +1,94 @@
 #!/bin/bash
-# Build openwrt in docker container
-
-# Enviroment
-GIT_BRANCH='openwrt-18.06'
-CORES='-j7'
-DEBUG='false'
+# Build openwrt
 
 # Stop on error
 set -e
 
-## Setup ccache
-# Update symlinks
+# Colors
+YELLOW='\033[33m'
+NC='\033[0m'
+
+# Enviroment
+echo -e "$YELLOW--> Setting up enviroment... $NC"
+GIT_URL='https://git.openwrt.org/Pheoxy/openwrt.git'
+GIT_BRANCH='openwrt-18.06'
+RESET_GIT='true'
+CORES='7'
+EXTRA_OPTIONS='true'
+DEBUG='true'
+
+# Setup ccache and update symlinks
 sudo /usr/sbin/update-ccache-symlinks
-
-# Prepend ccache into the PATH
-# if [[ -z "${/usr/lib/ccache:$PATH}" ]];
-#     then
-#         MY_SCRIPT_VARIABLE="Some default value because DEPLOY_ENV is undefined"
-#     else
-#         MY_SCRIPT_VARIABLE="${DEPLOY_ENV}"
-# fi
-# echo "printenv PATH" | find /C /I ";</usr/lib/ccache:$PATH>;"
-
-echo 'export PATH="/usr/lib/ccache:$PATH"' | tee -a ~/.bashrc
-
-# Source bashrc to test the new PATH
-source ~/.bashrc
+export PATH="/usr/lib/ccache:$PATH"
 
 # Git clone openwrt openwrt
+echo -e "$YELLOW--> Checking for source files... $NC"
 if [ ! -d openwrt/ ]
     then
-        git clone https://git.openwrt.org/openwrt/openwrt.git -b $GIT_BRANCH
+        echo -e "No source files found"
+        echo -e "$YELLOW--> Starting git clone for $GIT_URL $NC"
+        git clone $GIT_URL -b $GIT_BRANCH
         cd openwrt
 
         ./scripts/feeds update -a
         ./scripts/feeds install -a
     else
-        echo "Found openwrt/ directory from previous git clone"
-        echo "Skipping..."
-        echo "Pulling updates from git and cleaning openwrt/"
+        echo -e "Found existing source files"
+        echo -e "Updating source files"
 
         cd openwrt
         rm -rf feeds/*
         rm -rf package/*
 
-        git fetch origin
-        git reset --hard origin/$GIT_BRANCH
-        git clean -f -d
-        git pull
-
-        make dirclean
-
+        if [ $RESET_GIT = true ]
+            then
+                echo -e "$YELLOW--> Resetting git... $NC"
+                git fetch origin
+                git reset --hard origin/$GIT_BRANCH
+                git clean -f -d
+                git pull
+                make dirclean
+            else
+                echo -e "$YELLOW--> Cleaning up source... $NC"
+                make distclean
+        fi
         ./scripts/feeds update -a
         ./scripts/feeds install -a
 fi
-  
+
 # Setup .config from config.seed and update seed for new changes
+echo -e "$YELLOW--> Setup source for compile... $NC"  
 cp ../config.seed ../openwrt/.config
 make defconfig
 #make menuconfig
 
-
-# Compile
+# Prepare for multicore compile
+echo -e "$YELLOW--> Prepare for multicore compile... $NC"
 make download
 
 # Make output folders
+echo -e "$YELLOW--> Starting compile... $NC"
 mkdir -p ../output
 
-if [ $DEBUG=true ]
+# Check for debug and compile
+if [ $DEBUG = true ]
     then
-        time ionice -c 3 nice -n 20 make $CORES V=s 2>&1 | tee ../output/make.log | grep -i error
+        echo -e "Running with debug enabled"
+        rm -rf ../output/make.log
+        time ionice -c 3 nice -n 20 make -j1 V=s 2>&1 | tee ../output/make.log | grep -i error
     else
-        time ionice -c 3 nice -n19 make $CORES
+        time ionice -c 3 nice -n19 make -j$CORES
 fi
 
-# Cleaning up for git
+# Copying to output folder
+echo -e "$YELLOW--> Copying files to output folder... $NC"
 mkdir -p ../output/$(date +%Y%m%d%H%M)
 
-if [ -f ../output/make.log ]
+if [ -s "../output/make.log" ]
     then
         mv ../output/make.log ../output/$(date +%Y%m%d%H%M)/make.log
 fi
 
 cp -R bin/targets/* ../output/$(date +%Y%m%d%H%M)/
-
-echo "Build Success!"
 
 exit 0
